d0a18943e57fe9c342b61d4eca577c67d462f507
ticknotty <nicktotty8@gmail.com>
2015-11-02 13:34:00 +0000

    Initial commit

---
+++ display.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1202663
+ * Date: 02/11/2015
+ * Time: 13:32
+ */
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+echo "Log In Successful";
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE>
+<html>
+<head>
+</head>
+<body>
+<form action="display.php" method="post">
+<label>Username: </label>
+    <input type="text" name="username">
+    <br>
+<label>Password: </label>
+    <input type="password" name="password">
+</form>
+</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1202663
+ * Date: 02/11/2015
+ * Time: 13:23
+ */
